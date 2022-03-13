8c6f24f8040536f5a9a0d4b2f66071f4db99c83f
aditya sarfare <adityasarfare@gmail.com>
2022-03-13 16:03:25 +0530

    javascript added

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const bar = document.getElementById('bar');
-const close = document.getElementById('close');
-const nav = document.getElementById('navbar');
+// const bar = document.getElementById('bar');
+// const close = document.getElementById('close');
+// const nav = document.getElementById('navbar');
 
 
 if(bar){
